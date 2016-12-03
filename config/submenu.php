e86ebb5d03f6508e5ee1da82cd7bbfa673e4222a
aprel_co <sasha.g@w-not.ru>
2016-12-03 20:44:03 +0300

    submenu

--- config/submenu.php
+++ config/submenu.php
@@ -1,20 +1,23 @@
 <?php
+$aboutSubmenu = [
+    [
+        'link' => 'about',
+        'class' => 'ico3',
+        'text' => 'об агентстве'
+    ],
+    [
+        'link' => 'command',
+        'class' => 'ico4',
+        'text' => 'команда'
+    ],
+    [
+        'link' => 'clients',
+        'class' => 'ico5',
+        'text' => 'клиенты'
+    ],
+];
 return [
-    'about' => [
-        [
-            'link' => 'about',
-            'class' => 'ico3',
-            'text' => 'об агентстве'
-        ],
-        [
-            'link' => 'command',
-            'class' => 'ico4',
-            'text' => 'команда'
-        ],
-        [
-            'link' => 'clients',
-            'class' => 'ico5',
-            'text' => 'клиенты'
-        ],
-    ]
+    'about' => $aboutSubmenu,
+    'command' => $aboutSubmenu,
+    'clients' => $aboutSubmenu
 ];
