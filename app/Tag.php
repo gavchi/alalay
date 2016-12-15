db81211811373b59dc96adccc80b0226d290dcc7
Aleksandr Gavva <gavchi@gmail.com>
2016-12-15 10:52:19 +0300

    Add News relation for Tag

--- app/Tag.php
+++ app/Tag.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
+    
+    public function news()
+    {
+        return $this->belongsToMany('App\News', 'tag_news', 'tag_id', 'news_id');
+    }
 }
